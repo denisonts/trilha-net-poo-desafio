ad160fe04cd163fff2b827b7e64b769701eb97e3
Denison Silva <denisonts@hotmail.com>
2022-11-18 22:13:36 -0300

    Desafio "Criando um Sistema e Abstraindo um Celular com POO em C#"

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Aparelho Nokia - Testes");
+        Smartphone nokia = new Nokia(numero: "988224455", modelo: "N8", imei: "000111222", memoria: 8);
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Whatsapp");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Aparelho Iphone - Testes");
+        Smartphone iphone = new Iphone(numero: "955331177", modelo: "12 Pro", imei: "555666777", memoria: 128);
+        iphone.Ligar();
+        iphone.InstalarAplicativo("Telegram");
+    }
+}   
